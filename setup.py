f818f37edcd12b1162ba839791280da18b2ab0d5
Matthew Macdonald-Wallace <matt@mockingbirdconsulting.co.uk>
2019-04-18 17:18:19 +0100

    Add License and Setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyloraserver",
+    version="0.0.1",
+    author="Mockingbird Consulting Ltd",
+    author_email="info+pyloraserver@mockingbirdconsulting.co.uk",
+    description="A python library for interacting with Loraserver.io.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mockingbirdconsulting/pyloraserver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
